a1d19e0f8e9fa30f70ef23658fbbe6c43b4fb6aa
Jinkal Parmar <73112186+j100k-parmar@users.noreply.github.com>
2021-04-13 16:48:43 +0530

    portfolio

--- JavaScript/responsive.js
+++ JavaScript/responsive.js
@@ -1,5 +1,5 @@
 let items = document.querySelectorAll('.btn_project');
-let active = document.querySelectorAll('Active');
+let active = document.querySelectorAll('.Active');
 let pictures = document.querySelectorAll('.gallary img');
 
 items.forEach(button => {
@@ -14,8 +14,26 @@ items.forEach(button => {
             img.style.display = 'none';
             if (img.getAttribute('data-filter') == value.toLowerCase() || value == "All") {
                 img.style.display = 'block';
-                // img.style.float = 'left';
+                img.style.float = 'left';
             }
         })
     }
 });
+
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        x.style.width = "60%";
+        x.style.overflow = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+}
+
+
+// function demo(x) {
+//     x.classList.toggle("change");
+//     document.querySelector("#menu").classList.toggle('out');
+// }
